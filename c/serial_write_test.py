a3f5af38d295fc316aea9e419bf5199c10003ab9
rick <rick@ricks-MacBook-Pro.local>
2023-06-04 12:16:37 -0700

    add usb code

---
+++ c/serial_write_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import json, serial, time, sys
+ 
+port = sys.argv[1]
+print(f"Opening port {port}")
+ 
+
+js_chans = {
+	"pitch": 0,
+	"roll": 0,
+	"yaw": 0,
+	"throttle": 0,
+	"button0": 0,
+}
+
+# -------- Main Program Loop -----------
+with serial.Serial() as ser:
+  ser.baudrate = 115200
+  ser.port = port
+  ser.open()
+  print("opening serial port...")
+  time.sleep(2)
+  print("serial port ready...")
+  while True:
+    js_str = json.dumps(js_chans) + '\n'
+    print(f'json to send: {js_str}')
+    ser.write(bytes(js_str,'utf-8'))
+    print("sent to serial port")
