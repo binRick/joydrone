f4278b07c142ad37a455796385ccbac3c5df2ac9
rick <rick@ricks-MacBook-Pro.local>
2023-06-04 12:08:11 -0700

    add usb code

--- c/test_write_serial.c
+++ c/test_write_serial.c
@@ -7,7 +7,7 @@
 #include <termios.h>
 #define PORT "/dev/ttyUSB0"
 #define BAUD 115200
-#define DATA "{\"pitch\": 1898, \"yaw\": 996, \"roll\": 0, \"throttle\": 2000,\"button0\": 0}\n"
+#define DATA "{\"pitch\": 1898, \"yaw\": 996, \"roll\": 0, \"throttle\": 1000,\"button0\": 0}\n"
 
 
 int writeToSerialPort(const char* portPath, const char* data, speed_t baudRate);
