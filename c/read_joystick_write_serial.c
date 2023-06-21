020a9c6305a78d9a4cb4f4acb76fe64326b4df0e
Rick Blundell <4857149+binRick@users.noreply.github.com>
2023-06-21 07:33:05 -0400

    Update read_joystick_write_serial.c

--- c/read_joystick_write_serial.c
+++ c/read_joystick_write_serial.c
@@ -38,11 +38,6 @@ int main(int argc, const char * argv[]){
 	printf("No joystick found!\n");
 	exit(1);
     }
-
-    if (DEBUG_JOYSTICK_CHANNELS){
-    	printf("There are %zd devices found\n", list);
-    	print_devs(devs);
-    }
     
     libusb_free_device_list(devs, 1);
     libusb_exit(NULL);
@@ -64,34 +59,6 @@ int scaleValue(int value) {
     }
 
     return (int)((value / 255.0) * 2000);
-}
-
-
-void print_devs(libusb_device **devs){
-  libusb_device *dev;
-  int           i = 0, j = 0;
-  uint8_t       path[8];
-
-  while ((dev = devs[i++]) != NULL) {
-    struct libusb_device_descriptor desc;
-    int                             r = libusb_get_device_descriptor(dev, &desc);
-    if (r < 0) {
-      fprintf(stderr, "failed to get device descriptor");
-      return;
-    }
-
-    printf("%04x:%04x (bus %d, device %d)",
-           desc.idVendor, desc.idProduct,
-           libusb_get_bus_number(dev), libusb_get_device_address(dev));
-
-    r = libusb_get_port_numbers(dev, path, sizeof(path));
-    if (r > 0) {
-      printf(" path: %d", path[0]);
-      for (j = 1; j < r; j++)
-        printf(".%d", path[j]);
-    }
-    printf("\n");
-  }
 }
 
 int monitor_usb(void) {
